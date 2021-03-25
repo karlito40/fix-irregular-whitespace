a8955935d211afe32e317e7ef9569dcc93c5905d
Mohamed Lamine Allal <allalmohamedlamine@gmail.Com>
2021-03-25 17:39:52 +0100

    add: support all eslint irregular whitespaces
    Moving from handling only the (ALT+SPACE) to  handling of all eslit listed irregular whitespaces as by https://eslint.org/docs/rules/no-irregular-whitespace.

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,42 @@ import globby from 'globby';
 
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
-const irregularWhitespace = ' ';
+
+/*
+* Picked from no-irregular-whitespace rule implementation 
+* https://eslint.org/docs/rules/no-irregular-whitespace
+*/
+const irregularWhitespaces = [
+  '\u000B', // Line Tabulation (\v) - <VT>
+  '\u000C', // Form Feed (\f) - <FF>
+  '\u00A0', // No-Break Space - <NBSP>
+  '\u0085', // Next Line
+  '\u1680', // Ogham Space Mark
+  '\u180E', // Mongolian Vowel Separator - <MVS>
+  '\ufeff', // Zero Width No-Break Space - <BOM>
+  '\u2000', // En Quad
+  '\u2001', // Em Quad
+  '\u2002', // En Space - <ENSP>
+  '\u2003', // Em Space - <EMSP>
+  '\u2004', // Tree-Per-Em
+  '\u2005', // Four-Per-Em
+  '\u2006', // Six-Per-Em
+  '\u2007', // Figure Space
+  '\u2008', // Punctuation Space - <PUNCSP>
+  '\u2009', // Thin Space
+  '\u200A', // Hair Space
+  '\u200B', // Zero Width Space - <ZWSP>
+  '\u2028', // Line Separator
+  '\u2029', // Paragraph Separator
+  '\u202F', // Narrow No-Break Space
+  '\u205f', // Medium Mathematical Space
+  '\u3000' // Ideographic Space
+];
+
+const irregularWhitespacesMatchRegex = new RegExp(
+    `[${irregularWhitespaces.join('')}]`,
+    'g'
+);
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "fix-irregular-whitespace" is now active!');
@@ -60,7 +95,7 @@ export function activate(context: vscode.ExtensionContext) {
         return readFile(fsPath, 'utf-8').then(content => {
           let newText = removeIrregular(content);
           if (newText) {
-            return writeFile(fsPath, newText);
+            return writeFile(fsPath, newText as string);
           }
         });
       });
@@ -90,13 +125,13 @@ async function fixIrregularWhitespace (document: vscode.TextDocument) {
     return editor.edit(editBuilder => editBuilder.replace(fullRange, <string> newText));
   }
 
-  return writeFile(document.uri.fsPath, newText);
+  return writeFile(document.uri.fsPath, newText as string);
 }
 
 function removeIrregular (text: string): string | boolean {
-  if(!text.includes(irregularWhitespace)) {
+  if(!irregularWhitespacesMatchRegex.test(text)) {
     return false;
   }
 
-  return text.replace(new RegExp(irregularWhitespace, 'g'), ' '); // irregular whitespace
+  return text.replace(irregularWhitespacesMatchRegex, ' '); // irregular whitespace
 }
